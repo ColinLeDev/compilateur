7b2c562a496505c141f3ab4915a511937bb9aabf
Colin PALAZZETTI RUBERA <colin.palazzetti-rubera@alumni.univ-avignon.fr>
2024-04-11 16:06:17 +0200

    push

--- compilateur.cpp
+++ compilateur.cpp
@@ -7,17 +7,16 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 // Build with "make compilateur"
-
 
 #include <string>
 #include <iostream>
@@ -25,158 +24,169 @@
 
 using namespace std;
 
-char current, lookedAhead;                // Current char    
-int NLookedAhead=0;
+char current, lookedAhead; // Current char
+int NLookedAhead = 0;
 
-void ReadChar(void){
-    if(NLookedAhead>0){
-        current=lookedAhead;    // Char has already been read
-        NLookedAhead--;
-    }
-    else
-        // Read character and skip spaces until 
-        // non space character is read
-        while(cin.get(current) && (current==' '||current=='\t'||current=='\n'));
+void ReadChar(void)
+{
+	if (NLookedAhead > 0)
+	{
+		current = lookedAhead; // Char has already been read
+		NLookedAhead--;
+		return;
+	}
+	// Read character and skip spaces until
+	// non space character is read
+	while (cin.get(current) && (current == ' ' || current == '\t' || current == '\n'))
+		;
 }
 
-void LookAhead(void){
-    while(cin.get(lookedAhead) && (lookedAhead==' '||lookedAhead=='\t'||lookedAhead=='\n'));
-    NLookedAhead++;
+void LookAhead(void)
+{
+	while (cin.get(lookedAhead) && (lookedAhead == ' ' || lookedAhead == '\t' || lookedAhead == '\n'))
+		;
+	NLookedAhead++;
 }
 
-void Error(string s){
-	cerr<< s << endl;
+void Error(string s)
+{
+	cerr << s << endl;
 	exit(-1);
 }
 
-
-	
 // AdditiveOperator := "+" | "-"
-void AdditiveOperator(void){
-	if(current=='+'||current=='-')
+void AdditiveOperator(void)
+{
+	if (current == '+' || current == '-')
 		ReadChar();
 	else
-		Error("Opérateur additif attendu");	   // Additive operator expected
-}
-		
-// Number := "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"
-void Number(void){
-	if((current<'0')||(current>'9'))
-		Error("Chiffre attendu");		   // Number expected
-	else{
-		string number;
-		number.append(1,current);
-		cerr << "Number : "<<number<<endl;
-		LookAhead();  // Look the next char to see if it's a digit
-		cerr << "Char to test : "<<lookedAhead<<endl;
-		while((lookedAhead>='0')&&(lookedAhead<='9')){ // If it's a digit, we add it to the number
-			number.append(1,lookedAhead); 
-			cerr << "Number : "<<number<<endl; 
-			LookAhead(); // Prepare the next step of the while
-			// ReadChar(); // Cancel the previous LookAhead (to keep NLookedAhead at 1)
-			cerr << "Char to test : "<<lookedAhead<<endl;
-		}
-		ReadChar();
-		cout << "\tpush $"<<number<<endl;
-	}
+		Error("Opérateur additif attendu"); // Additive operator expected
 }
 
-void ArithmeticExpression(void);			// Called by Term() and calls Term()
+// Letter := "a"|"b"|"c"|"d"|"e"|"f"|"g"|"h"|"i"|"j"|"k"|"l"|"m"|"n"|"o"|"p"|"q"|"r"|"s"|"t"|"u"|"v"|"w"|"x"|"y"|"z"
+
+// Digit := "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"
+// Number := Digit {Digit}
+void Number(void)
+{
+	if ((current < '0') || (current > '9'))
+		Error("Chiffre attendu"); // Number expected
+		
+	unsigned long long number = current - '0';
+	ReadChar();
+	while ((current >= '0') && (current <= '9'))
+	{
+		number *= 10;
+		number += current - '0';
+		ReadChar();
+	}
+	cout << "\tpush $"<< number << endl;
+}
+
+void ArithmeticExpression(void); // Called by Term() and calls Term()
 
 // Term := Number | "(" ArithmeticExpression ")"
-void Term(void){
-	if(current=='('){
+void Term(void)
+{
+	if (current == '(')
+	{
 		ReadChar();
 		ArithmeticExpression();
-		if(current!=')')
-			Error("')' était attendu");		// ")" expected
+		if (current != ')')
+			Error("')' était attendu"); // ")" expected
 		else
 			ReadChar();
 	}
-	else 
-		if (current>='0' && current <='9')
-			Number();
-	     	else
-			Error("'(' ou chiffre attendu");
+	else if (current >= '0' && current <= '9')
+		Number();
+	else
+		Error("'(' ou chiffre attendu");
 }
 
 // ArithmeticExpression := Term {AdditiveOperator Term}
-void ArithmeticExpression(void){
+void ArithmeticExpression(void)
+{
 	char adop;
 	Term();
-	while(current=='+'||current=='-'){
-		adop=current;		// Save operator in local variable
+	while (current == '+' || current == '-')
+	{
+		adop = current; // Save operator in local variable
 		AdditiveOperator();
 		Term();
-		cout << "\tpop %rbx"<<endl;	// get first operand
-		cout << "\tpop %rax"<<endl;	// get second operand
-		if(adop=='+')
-			cout << "\taddq	%rbx, %rax"<<endl;	// add both operands
+		cout << "\tpop %rbx" << endl; // get first operand
+		cout << "\tpop %rax" << endl; // get second operand
+		if (adop == '+')
+			cout << "\taddq	%rbx, %rax" << endl; // add both operands
 		else
-			cout << "\tsubq	%rbx, %rax"<<endl;	// substract both operands
-		cout << "\tpush %rax"<<endl;			// store result
+			cout << "\tsubq	%rbx, %rax" << endl; // substract both operands
+		cout << "\tpush %rax" << endl;				 // store result
 	}
 }
 
-char OpRel(void){	// Relational operator
-	if(current!='='&&current!='<'&&current!='>'&&current!='!')
+char OpRel(void)
+{ // Relational operator
+	if (current != '=' && current != '<' && current != '>' && current != '!')
 		Error("Opérateur relationnel attendu");
-	char oprel=current;
+	char oprel = current;
 	ReadChar();
 	return oprel;
 }
 // ExpA == Number := "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"
-void ExpA(void){
+void ExpA(void)
+{
 	// Since I want to compare arithmetical expressions, I need to call the function ArithmeticExpression
 	ArithmeticExpression();
 	return; // Block Number because it's replaced by ArithmeticExpression
 	Number();
 }
 
-void Exp(void){
+void Exp(void)
+{
 	ExpA();
-	if(current=='<'||current=='>'||current=='='||current=='!'){
+	if (current == '<' || current == '>' || current == '=' || current == '!')
+	{
 		char oprel = OpRel();
 		ExpA();
-		cout << "\tpop %rax"<<endl;
-		cout << "\tpop %rbx"<<endl;
-		cout << "\tcmpq %rax, %rbx"<<endl;
+		cout << "\tpop %rax" << endl;
+		cout << "\tpop %rbx" << endl;
+		cout << "\tcmpq %rax, %rbx" << endl;
 		switch (oprel)
 		{
 		case '<':
-			cout << "\tjb Vrai\t# Aller sur code de vrai"<<endl;
+			cout << "\tjb Vrai\t# Aller sur code de vrai" << endl;
 			break;
 
 		case '>':
-			cout << "\tja Vrai\t# Aller sur code de vrai"<<endl;
+			cout << "\tja Vrai\t# Aller sur code de vrai" << endl;
 			break;
 
 		case '=':
-			cout << "\tje Vrai\t# Aller sur code de vrai"<<endl;
+			cout << "\tje Vrai\t# Aller sur code de vrai" << endl;
 			break;
 
 		case '!':
-			cout << "\tjne Vrai\t# Aller sur code de vrai"<<endl;
+			cout << "\tjne Vrai\t# Aller sur code de vrai" << endl;
 			break;
-		
+
 		default:
 			Error("Opérateur relationnel attendu - default triggered");
 			break;
 		}
-		cout << "Faux:\tpush $0\t# Faux sur la pile"<<endl;
-		cout << "\tjmp FinExp\t# Aller à Fin, évider code faux"<<endl;
-		cout << "Vrai:\tpush $-1\t# Code de vrai"<<endl;
+		cout << "Faux:\tpush $0\t# Faux sur la pile" << endl;
+		cout << "\tjmp FinExp\t# Aller à Fin, évider code faux" << endl;
+		cout << "Vrai:\tpush $-1\t# Code de vrai" << endl;
 		cout << "FinExp:";
 	}
 }
 
-int main(void){	// First version : Source code on standard input and assembly code on standard output
+int main(void)
+{ // First version : Source code on standard input and assembly code on standard output
 	// Header for gcc assembler / linker
-	cout << "\t\t\t# This code was produced by the CERI Compiler"<<endl;
-	cout << "\t.text\t\t# The following lines contain the program"<<endl;
-	cout << "\t.globl main\t# The main function must be visible from outside"<<endl;
-	cout << "main:\t\t\t# The main function body :"<<endl;
-	cout << "\tmovq %rsp, %rbp\t# Save the position of the stack's top"<<endl;
+	cout << "\t\t\t# This code was produced by the CERI Compiler" << endl;
+	cout << "\t.text\t\t# The following lines contain the program" << endl;
+	cout << "\t.globl main\t# The main function must be visible from outside" << endl;
+	cout << "main:\t\t\t# The main function body :" << endl;
+	cout << "\tmovq %rsp, %rbp\t# Save the position of the stack's top" << endl;
 
 	// Let's proceed to the analysis and code production
 	ReadChar();
@@ -184,18 +194,11 @@ int main(void){	// First version : Source code on standard input and assembly co
 	Exp();
 	ReadChar();
 	// Trailer for the gcc assembler / linker
-	cout << "\tmovq %rbp, %rsp\t\t# Restore the position of the stack's top"<<endl;
-	cout << "\tret\t\t\t# Return from main function"<<endl;
-	if(cin.get(current)){
-		cerr <<"Caractères en trop à la fin du programme : ["<<current<<"]";
+	cout << "\tmovq %rbp, %rsp\t\t# Restore the position of the stack's top" << endl;
+	cout << "\tret\t\t\t# Return from main function" << endl;
+	if (cin.get(current))
+	{
+		cerr << "Caractères en trop à la fin du programme : [" << current << "]";
 		Error("."); // unexpected characters at the end of program
 	}
-
 }
-		
-			
-
-
-
-
-
